8f85eb7044a819990fc7b81974bb03f3d543bb78
tariqmu7 <tariq_mu7amad@yahoo.com>
2025-12-22 01:35:55 +0200

    Update docs build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PRT/', //
+  base: '/PRT/',
+  build: {
+    outDir: 'docs', // This puts the finished website in a folder GitHub can see
+  }
 })
